ce8c14ec7bb53fb2bdce223a1ca9c5a7bad861d7
sunba <nsg2318@naver.com>
2022-02-25 18:08:12 +0900

    add : programers 추가

---
+++ algorithm/src/programmers/pgm42840/Main.java
@@ -0,0 +1,9 @@
+package programmers.pgm42840;
+
+public class Main {
+  public static void main(String[] args) {
+    Solution solution = new Solution();
+    int[] a = {3,1,3};
+    solution.solution(a);
+  }
+}
