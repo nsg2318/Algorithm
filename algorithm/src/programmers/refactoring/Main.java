fda46926a9a3e309d2aa6754ea63a30143f6ac98
sunba <nsg2318@naver.com>
2022-03-12 14:30:31 +0900

    add : 코테

---
+++ algorithm/src/programmers/refactoring/Main.java
@@ -0,0 +1,8 @@
+package programmers.refactoring;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        solution.solution("qyyigoptvfb","abcdefghijk",3);
+    }
+}
